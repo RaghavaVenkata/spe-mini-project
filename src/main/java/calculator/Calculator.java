b8ea79e882d61da1d24c7a1ef77389b618096ed5
RaghavaVenkata <venkataraghava7777@gmail.com>
2022-04-18 17:08:09 +0530

    changes made3

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,115 +8,156 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Calculator {
-
+    // creating a public class of the calculator function.
+    
     public static final Logger logger = LogManager.getLogger(Calculator.class);
-
+    // creating a public caluclator
+    
     public Calculator() {}
 
-    public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        Scanner scanner = new Scanner(System.in);
-        double num1, num2;
+    public static void main(String[] args) 
+    {
+        Calculator cal = new Calculator();   
+        Scanner scanner = new Scanner(System.in); 
+        double a, b; // double a (for first input ), and b (for second input)
 
         do{
             System.out.println("Calcuator");
-            System.out.println("Available functions\n1 - Square Root of x\n2 - Factorial of x\n3 - Natural Log of x\n4 - x Power b\nAny other number to exit");
-            int fun;
+            // printing calculator
+            System.out.println("Ava functions\n1 - Sqr Root of x\n2 - Fact of x\n3 - N Log of x\n4 - x Power b\nAny other number to exit");
+            //print avalible function ,square root,factorial,natrual log  
+            int function;
 
-            try{
-                fun = scanner.nextInt();
-            } catch (InputMismatchException error) {
+            try
+            {
+                function = scanner.nextInt();
+            } catch (InputMismatchException error) 
+            
+            {   // checking weather input is correct or wrong
                 return;
             }
 
-            if (fun < 1 || fun > 4){
+            if (function < 1 || function > 4)
+            {   // checking function range
                 return;
             }
 
-            try{
-                System.out.println("Enter x");
-                num1 = scanner.nextDouble();
-            } catch (InputMismatchException error) {
+            try
+            {   // entering the first value 
+                System.out.println("Enter value x");
+                a = scanner.nextDouble();
+            } catch (InputMismatchException error) 
+            {   // checking weather input is correct or wrong
                 return;
             }
 
-            switch(fun){
-                case 1:
-                    System.out.println("Square root of x = " + cal.sqt(num1));
-                    break;
-                case 2:
-                    System.out.println("Factorial of x =" + cal.factorial(num1));
-                    break;
-                case 3:
-                    System.out.println("Natural log of x =" + cal.log(num1));
-                    break;
-                case 4:
-                    try {
-                        System.out.print("Enter b");
-                        num2 = scanner.nextDouble();
-                    } catch (InputMismatchException error){
-                        return;
-                    }
-                    System.out.println("x power b = " + cal.power(num1, num2));
-                    break;
-                default:
-                    System.out.print("Closing");
+            if(function == 1)
+            {   // Square root of the x
+                System.out.println("Sqr root of x = " + cal.sqt(a));
+                break;
+            }
+            
+            else if(function == 2)
+            {   // Factorial root of the x 
+                System.out.println("Fact of x =" + cal.factorial(a));
+                break;
+            }
+            
+            else if(function == 3)
+            {   // Natural log of the x 
+                System.out.println("N log of x =" + cal.log(a));
+                break;
+            }
+            
+            else if(function == 4)
+            {
+                try 
+                {   // entering the second value
+                    System.out.print("Enter b");
+                    b = scanner.nextDouble();
+                } catch (InputMismatchException error)
+                {   // checking weather input is correct or wrong
                     return;
+                }
+                System.out.println("x power b = " + cal.power(a, b));
+                break;
+            }
+            else
+            {   // printing closing
+                System.out.print("Closing");
+                return;
             }
         }while(true);
     }
 
 
     public static double sqt(double i){
+        
         double temp = 0;
+        
         try {
             logger.info("[SQRT] - " + i);
-            if (i < 0) {
+            // if the value is less than 1
+            if (i < 0) 
+            { //printing Cannot find sqrt of -ve numbers
                 temp = Double.NaN;
-                throw new ArithmeticException("Cannot find square root of -ve numbers");
-            } else {
+                throw new ArithmeticException("ERROR of find square root of -ve numbers");
+            } 
+            else 
+            {
                 temp = Math.sqrt(i);
             }
-        } catch (ArithmeticException error){
-            logger.error("[EXCEPTION - SQRT] - Cannot find sqrt of -ve numbers");
-        } finally {
+        } catch (ArithmeticException error)
+        { //printing error Cannot find sqrt of -ve numbers
+            logger.error("[EXCEPTION - SQRT] - ERROR Cannot find sqrt of -ve numbers");
+        } finally 
+        { // printing results
             logger.info("[RESULT - SQRT] = " + temp);
         }
         return temp;
     }
 
-    public static double factorial(double i){
+    public static double factorial(double i)
+    {
         logger.info("[FACTORIAL] - " + i );
         double temp = 1.0;
-        for (int t = 1; t<= i; t++){
+        for (int t = 1; t<= i; t++)
+        {
             temp *= t;
         }
-        logger.info("[RESULT - FACTORIAL] = " + temp);
+        logger.info("[RESULT - FACTORIAL] = " + temp); // printing the factorial results
         return temp;
     }
-    public static double log(double i){
+    public static double log(double i)
+    {
         Double temp = 0.0;
         try {
             logger.info("[LOG] - " + i);
-            if (i < 0){
+            if (i < 0)
+            {   //printing Cannot find N Log of -ve numbers
                 temp = Double.NaN;
-                throw new ArithmeticException("Cannot find log of negative numbers");
+                throw new ArithmeticException("ERROR: Can't find log of negative numbers");
             }
-            else if (i == 0){
+            else if (i == 0)
+            {   //printing Cannot find N Log of 0 numbers
                 temp = Double.NEGATIVE_INFINITY;
                 throw new ArithmeticException("Log of 0 is negative infinity");
             }
-            else {
+            else 
+            {
                 temp = Math.log(i);
             }
-        } catch (ArithmeticException error){
-            logger.error("[EXCEPTION - log] - inout is less than or equal to zero");
-        } finally {
+        } catch (ArithmeticException error)
+        {   //printing ERROR Cannot find N Log  of lessthan or equal to zero numbers
+            logger.error("[EXCEPTION - log] - ERROR :inout is less than or equal to zero");
+        } finally 
+        {   // printing the Result
             logger.info("[RESULT - LOG] = " + temp);
         }
         return temp;
     }
-    public static double power(double i, double j){
+    public static double power(double i, double j)
+    {   // checking the power of given input
         logger.info("[POWER] - " + i + ", " + j );
         double temp = Math.pow(i,j);
         logger.info("[RESULT - POWER] = " + temp);
